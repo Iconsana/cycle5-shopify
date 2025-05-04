124fd6ff2c5cc3629477d763766f9cd13afc9a49
GhrollorhG <39232926+Iconsana@users.noreply.github.com>
2025-05-04 10:11:12 +0200

    Create templateRoutes.js

---
+++ server/templateRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const templateController = require('../controllers/templateController');
+const multer = require('multer');
+const upload = multer({ storage: multer.memoryStorage() });
+
+// Get all templates
+router.get('/', templateController.getAllTemplates);
+
+// Get a specific template
+router.get('/:id', templateController.getTemplateById);
+
+// Generate an image from a template
+router.post('/generate', upload.single('productImage'), templateController.generateImage);
+
+module.exports = router;
