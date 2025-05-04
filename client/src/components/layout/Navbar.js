524c000d9d080ea8d6da2361d41b8fc8e9dcf895
GhrollorhG <39232926+Iconsana@users.noreply.github.com>
2025-05-04 14:20:14 +0200

    Create Navbar.js

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import CompanyContext from '../../context/CompanyContext';
+
+const Navbar = () => {
+  const { companyData } = useContext(CompanyContext);
+  
+  return (
+    <nav className="navbar">
+      <div className="container navbar-container">
+        <Link to="/" className="logo">
+          {companyData.name} Template Generator
+        </Link>
+        
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/generator">Create Templates</Link>
+          <Link to="/settings">Settings</Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
