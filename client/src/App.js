d613d99c131b3e3387ea01656be7001fa32b9e28
GhrollorhG <39232926+Iconsana@users.noreply.github.com>
2025-05-04 11:10:01 +0200

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Home from './pages/Home';
+import TemplateGenerator from './pages/TemplateGenerator';
+import Settings from './pages/Settings';
+import { CompanyProvider } from './context/CompanyContext';
+import './App.css';
+
+function App() {
+  return (
+    <CompanyProvider>
+      <Router>
+        <div className="app">
+          <Navbar />
+          <main className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/generator" element={<TemplateGenerator />} />
+              <Route path="/settings" element={<Settings />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </CompanyProvider>
+  );
+}
+
+export default App;
