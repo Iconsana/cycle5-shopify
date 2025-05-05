fd2ee4ceff8fc266b46bf855a54ec274e2b4c956
GhrollorhG <39232926+Iconsana@users.noreply.github.com>
2025-05-05 04:55:07 +0200

    Update TemplateGenerator.js

--- client/src/pages/TemplateGenerator.js
+++ client/src/pages/TemplateGenerator.js
@@ -1,9 +1,10 @@
-  import TemplateSelector from '../components/templates/TemplateSelector';
-  import ProductForm from '../components/products/ProductForm';
-  import TemplatePreview from '../components/templates/TemplatePreview';
-  import ExportOptions from '../components/export/ExportOptions';
-  import CompanyContext from '../context/CompanyContext';
-  import { getTemplates } from '../services/templateService';
+import React, { useState, useEffect, useContext } from 'react';
+import TemplateSelector from '../components/templates/TemplateSelector';
+import ProductForm from '../components/products/ProductForm';
+import TemplatePreview from '../components/templates/TemplatePreview';
+import ExportOptions from '../components/export/ExportOptions';
+import CompanyContext from '../context/CompanyContext';
+import { getTemplates } from '../services/templateService';
 
 const TemplateGenerator = () => {
   const [templates, setTemplates] = useState([]);
